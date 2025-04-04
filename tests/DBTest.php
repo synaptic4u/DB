0157c8d2a9c77d8159abd8346d16e2be51d79428
Emile De Wilde <synaptic4u@gmail.com>
2025-04-04 13:51:40 +0200

    updated version

--- tests/DBTest.php
+++ tests/DBTest.php
@@ -11,13 +11,16 @@
  */
 
 /**
- * Checks for the existence of the Composer autoload file two directories up
+ * Checks for the existence of the Composer autoload file two or four directories up
  * from the current file's location. If it exists, includes it to enable
  * autoloading of classes and dependencies.
  */
-if (file_exists(dirname(__FILE__, 2) . '/vendor/autoload.php')) {
+if (file_exists(dirname(__FILE__,4) . '/autoload.php')) {
+    require_once dirname(__FILE__,4) . '/autoload.php';
+    // var_dump(dirname(__FILE__, 4).'/autoload.php');
+}else if (file_exists(dirname(__FILE__, 2) . '/vendor/autoload.php')) {
     require_once dirname(__FILE__, 2) . '/vendor/autoload.php';
-    // var_dump(dirname(__FILE__, 1).'/vendor/autoload.php');
+    // var_dump(dirname(__FILE__, 2).'/vendor/autoload.php');
 }
 
 use Synaptic4U\DB\DB;
